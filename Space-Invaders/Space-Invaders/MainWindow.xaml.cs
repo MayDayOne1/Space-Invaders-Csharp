7f3f61ff03757b9fbb403f93fa781c9fae530d7d
pmilowski <pmilowski16@gmail.com>
2023-05-08 09:11:15 +0200

    Initial setup
    Imported images
    Changes in <Title>
    Added Canvas
    Added variables

--- Space-Invaders/Space-Invaders/MainWindow.xaml.cs
+++ Space-Invaders/Space-Invaders/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Space_Invaders
 {
@@ -20,9 +21,45 @@ namespace Space_Invaders
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool goLeft, goRight = false;
+        private List<Rectangle> itemsToRemove = new List<Rectangle>();
+        private int enemyImages = 0;
+        private int bulletTimer;
+        private int bulletTimerLimit = 90;
+        private int totalEnemies;
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        ImageBrush playerSkin = new ImageBrush();
+        int enemySpeed = 6;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Canvas_KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Canvas_KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void enemyBulletSpawner(double x, double y)
+        {
+
+        }
+
+        private void spawnEnemies(int limit)
+        {
+
+        }
+
+        private void gameManager(object sender, EventArgs e)
+        {
+
         }
     }
 }
